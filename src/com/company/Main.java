ad39b525b99659687aa5cd809ae953f1b21fca6e
voryba <erasabyr14@gmail.com>
2021-03-07 19:00:52 +0600

    final commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        UserRepository repo = new UserRepository();
-        repo.transfer("asdasd","yernur",500);
+        Application app=new Application();
+        app.start();
     }
 }
